78f1bdc895458e286f258c78d34b60e44fecc658
Andrew Hallberg & Kelly Becker <student@epicodus.com>
2014-02-21 10:51:49 -0800

    creates first final version of scrabble score finder

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
-var cryptoDecoder = function(string) {
-  var splitWordArray = string.split(" ");
-  var joinWord = splitWordArray.join("");
+var wordScore = function(word) {
+  var letterArr = word.split("");
+  var total = 0
+  for (var i=0; i<letterArr.length; i++) {
+    total += letterScore(letterArr[i]);
+  } 
+  return total;
+}
 
-  var sevenCharacterWords = joinWord.match(/.{1,7}/g); //this is now an array of 7 characters
+var letterScore = function(letter) {
+  var letter = letter.toUpperCase();
+  var score1Letters = ["A","E","I","O","U","L","N","R","S","T"];
+  var score2Letters = ["D", "G"];
+  var score3Letters = ["B","C","M","P"];
+  var score4Letters = ["F", "H", "V", "W", "Y"];
+  var score5Letters = ["K"];
+  var score8Letters = ["J","X"];
+  var score10Letters = ["Q","Z"];
+  var scoreLetter = 0
 
-  return sevenCharacterWords;
-
-  var splitWordNoSpace = joinWord.split("");
-  var Apple = splitWordNoSpace;
-  return Apple;
-
-
-
+  for (var i = 0; i < score1Letters.length; i++) {
+    if (letter === score1Letters[i]) {
+      scoreLetter = 1;
+    }else if (letter === score2Letters[i]) {
+      scoreLetter = 2;
+    }else if (letter === score3Letters[i]) {
+      scoreLetter = 3;
+    }else if (letter === score4Letters[i]) {
+      scoreLetter = 4;
+    }else if (letter === score5Letters[i]) {
+      scoreLetter = 5;
+    }else if (letter === score8Letters[i]) {
+      scoreLetter = 8;
+    }else if (letter === score10Letters[i]) {
+      scoreLetter = 10;  
+    }
+  };    
+    
+  return scoreLetter;
 
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,41 @@
-describe('cryptoDecoder', function(string) {
-  it('Takes a string and splits the characters into an array without spaces', function() {
-  cryptoDecoder("bigmac").should.eql(['b', 'i', 'g', 'm', 'a', 'c']);
+describe('letterScore', function() {
+  
+  it('gives us a score of 1 for "a"', function() {
+    letterScore("a").should.equal(1);
   });
+
+  it('returns a score of 1 for any letter that should be scored as a one', function() {
+    letterScore("e").should.equal(1);
+  });
+
+  it('returns a score of 2 for any letter that should be scored as 2', function() {
+    letterScore("d").should.equal(2);
+  });
+
+  it('returns a score of 3 for any letter that should be scored as 3', function() {
+    letterScore("b").should.equal(3);
+  });
+
+  it('returns a score of 4 for any letter that should be scored as 4', function() {
+    letterScore("y").should.equal(4);
+  });
+
+  it('returns the correct single letter score when a letter is used in the fuction', function () {
+    letterScore("z").should.equal(10);
+  });
+
+});
+
+describe('wordScore', function() {
+  
+  it('gives us a score of 1 for "a"', function() {
+    wordScore("a").should.equal(1);
+  });
+
+  it('it gives us the correct score for a two-letter word', function() {
+    wordScore("high").should.equal(11);
+  });
+
 });
 
 
