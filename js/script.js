b5c0286cb0ed095ae004f00360ac588eeb80429f
Andrew Hallberg & Kelly Becker <student@epicodus.com>
2014-02-21 12:28:26 -0800

    adds object scoreArr for scores

--- js/script.js
+++ js/script.js
@@ -9,32 +9,35 @@ var wordScore = function(word) {
 
 var letterScore = function(letter) {
   var letter = letter.toUpperCase();
-  var score1Letters = ["A","E","I","O","U","L","N","R","S","T"];
-  var score2Letters = ["D", "G"];
-  var score3Letters = ["B","C","M","P"];
-  var score4Letters = ["F", "H", "V", "W", "Y"];
-  var score5Letters = ["K"];
-  var score8Letters = ["J","X"];
-  var score10Letters = ["Q","Z"];
+  var scoreArrs = {
+      score1Letters : ["A","E","I","O","U","L","N","R","S","T"],
+      score2Letters :["D", "G"],
+      score3Letters :["B","C","M","P"],
+      score4Letters :["F", "H", "V", "W", "Y"],
+      score5Letters :["K"],
+      score8Letters :["J","X"],
+      score10Letters: ["Q","Z"]  
+  };
+  
 
-  if (score1Letters.indexOf(letter) >=0) {
+  if (scoreArrs.score1Letters.indexOf(letter) >=0) {
     return 1;
-  } else if (score2Letters.indexOf(letter) >=0){
+  } else if (scoreArrs.score2Letters.indexOf(letter) >=0){
     return 2; 
-  } else if (score3Letters.indexOf(letter) >=0) {
+  } else if (scoreArrs.score3Letters.indexOf(letter) >=0) {
     return 3;
-  } else if (score4Letters.indexOf(letter) >=0) {
+  } else if (scoreArrs.score4Letters.indexOf(letter) >=0) {
     return 4;
-  } else if (score5Letters.indexOf(letter) >=0) {
+  } else if (scoreArrs.score5Letters.indexOf(letter) >=0) {
     return 5;
-  } else if (score8Letters.indexOf(letter) >=0) {
+  } else if (scoreArrs.score8Letters.indexOf(letter) >=0) {
     return 8;
-  } else if (score10Letters.indexOf(letter) >=0) {
+  } else if (scoreArrs.score10Letters.indexOf(letter) >=0) {
     return 10;
   }
 };
 
-  //Before Refactoring with .idexOf
+  //Before Refactoring with .indexOf
   // for (var i = 0; i < score1Letters.length; i++) {
   //   if (letter === score1Letters[i]) {
   //     scoreLetter = 1;
