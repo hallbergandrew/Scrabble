7cf3f18080f3ef4923f6fa92fc389e25a6c23770
Andrew Hallberg & Kelly Becker <student@epicodus.com>
2014-02-21 11:08:21 -0800

    refactored with .indexOf

--- js/script.js
+++ js/script.js
@@ -16,26 +16,41 @@ var letterScore = function(letter) {
   var score5Letters = ["K"];
   var score8Letters = ["J","X"];
   var score10Letters = ["Q","Z"];
-  var scoreLetter = 0
 
-  for (var i = 0; i < score1Letters.length; i++) {
-    if (letter === score1Letters[i]) {
-      scoreLetter = 1;
-    }else if (letter === score2Letters[i]) {
-      scoreLetter = 2;
-    }else if (letter === score3Letters[i]) {
-      scoreLetter = 3;
-    }else if (letter === score4Letters[i]) {
-      scoreLetter = 4;
-    }else if (letter === score5Letters[i]) {
-      scoreLetter = 5;
-    }else if (letter === score8Letters[i]) {
-      scoreLetter = 8;
-    }else if (letter === score10Letters[i]) {
-      scoreLetter = 10;  
-    }
-  };    
-    
-  return scoreLetter;
+  if (score1Letters.indexOf(letter) >=0) {
+    return 1;
+  } else if (score2Letters.indexOf(letter) >=0){
+    return 2; 
+  } else if (score3Letters.indexOf(letter) >=0) {
+    return 3;
+  } else if (score4Letters.indexOf(letter) >=0) {
+    return 4;
+  } else if (score5Letters.indexOf(letter) >=0) {
+    return 5;
+  } else if (score8Letters.indexOf(letter) >=0) {
+    return 8;
+  } else if (score10Letters.indexOf(letter) >=0) {
+    return 10;
+  }
+
+  //Before Refactoring with .idexOf
+  // for (var i = 0; i < score1Letters.length; i++) {
+  //   if (letter === score1Letters[i]) {
+  //     scoreLetter = 1;
+  //   }else if (letter === score2Letters[i]) {
+  //     scoreLetter = 2;
+  //   }else if (letter === score3Letters[i]) {
+  //     scoreLetter = 3;
+  //   }else if (letter === score4Letters[i]) {
+  //     scoreLetter = 4;
+  //   }else if (letter === score5Letters[i]) {
+  //     scoreLetter = 5;
+  //   }else if (letter === score8Letters[i]) {
+  //     scoreLetter = 8;
+  //   }else if (letter === score10Letters[i]) {
+  //     scoreLetter = 10;  
+  //   }
+  // };    
+
 
 };
